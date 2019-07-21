bfbfcef5d629b9f8dfdf50c8e00d6bb57a61cddd
charles agyemang <micnkru@gmail.com>
2019-07-21 13:44:40 +0000

    Feat:

---
+++ lib/screens/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'web_view_container.dart';
+
+class Home extends StatelessWidget {
+  static final links = [ ['https://google.com', 'Google'], ['https://pianoafrikonline.com', 'Pianoafrik'] ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: Key('123'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children:
+                links.map((link) => _urlButton(context, link[0], link[1])).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _urlButton(BuildContext context, String url, String title) {
+    return Container(
+        padding: EdgeInsets.all(20.0),
+        child: FlatButton(
+          color: Theme.of(context).primaryColor,
+          padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+          child: Text(title),
+          onPressed: () => _handleURLButtonPress(context, url),
+        ));
+  }
+
+  void _handleURLButtonPress(BuildContext context, String url) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => WebViewContainer(url)));
+  }
+}
